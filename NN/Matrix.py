f65a297d47d58e5ce85a0d46c628cf58ea64b751
Pranav Rao <prawnrao97@gmail.com>
2020-04-08 16:07:52 +0530

    PEP8-ified both Matrix.py and NeuralNet.py

--- NN/Matrix.py
+++ NN/Matrix.py
@@ -1,64 +1,66 @@
 from random import random
 
 class Matrix(object):
-    """ Matrix object which is used to compute math needed for the NeuralNet class."""
+    """ Matrix object which is used to compute math
+        needed for the NeuralNet class.
+    """
     def __init__(self, rows: int, cols: int):
         self.__rows = rows
         self.__cols = cols
         self.__shape = (rows, cols)
-        
+
         self.data = [0] * rows
         for i in range(rows):
             self.data[i] = [0] * cols
-    
+
     def __mul__(self, val):
         """ Scalar multiplication"""
         return self.apply(lambda x: x * val)
-    
+
     def __rmul__(self, val):
         return self.__mul__(val)
-    
+
     def __add__(self, val):
         """ Scalar addition"""
         return self.apply(lambda x: x + val)
-    
+
     def __radd__(self, val):
         return self.__add__(val)
-    
+
     def __pow__(self, val):
         """ Scalar power"""
         return self.apply(lambda x: x ** val)
-    
+
     def __repr__(self):
         """ Return method"""
         string = "Matrix {}".format(self.shape)
         for i in range(self.rows):
             string += "\n {}".format(self.data[i])
-        return string 
-    
+        return string
+
     def __str__(self):
         """ To string method"""
         string = ""
-        
+
         for i in range(self.rows):
             string += "{} \n".format(self.data[i])
-        return string 
-    
+        return string
+
     @property
     def rows(self):
         """ Number of rows"""
         return self.__rows
-    
+
     @property
     def cols(self):
         """ Number of columns"""
         return self.__cols
-    
+
     @property
     def shape(self):
         """ (rows, columns)"""
         return self.__shape
-    
+
     @property
     def T(self):
         """ Transpose of Matrix"""
@@ -67,7 +69,7 @@ class Matrix(object):
             for j in range(m.cols):
                 m.data[i][j] = self.data[j][i]
         return m
-    
+
     @property
     def trace(self):
         """ Trace of Matrix"""
@@ -75,7 +77,7 @@ class Matrix(object):
         for i in range(self.rows):
             Tr += self.data[i][i]
         return Tr
-    
+
     @property
     def total(self):
         """ Sum of all elements"""
@@ -84,22 +86,22 @@ class Matrix(object):
             for j in range(self.cols):
                 tot += self.data[i][j]
         return tot
-    
+
     @property
     def negative(self):
         """ Negative of matrix"""
         return self * -1
-    
+
     @property
     def __min(self):
         """ Minimum element of matrix"""
         return min(self.to_array())
-    
+
     @property
     def __max(self):
         """ Maximum element of matrix"""
         return max(self.to_array())
-    
+
     def add(self, b, inplace=False):
         """ Sum of 2 matrices, can return or perform inplace"""
         if not inplace:
@@ -111,14 +113,14 @@ class Matrix(object):
                     else:
                         m.data[i][j] = self.data[i][j] + b.data[i][j]
             return m
-        
+
         for i in range(self.rows):
             for j in range(self.cols):
                 if b.cols == 1:
                     self.data[i][j] = self.data[i][j] + b.data[i][0]
                 else:
                     self.data[i][j] = self.data[i][j] + b.data[i][j]
-    
+
     def to_array(self):
         """ Matrix to array"""
         arr = []
@@ -133,14 +135,16 @@ class Matrix(object):
         """
         if maximum is None:
             maximum = self.__max
-            
+
         if minimum is None:
             minimum = self.__min
-            
+
         return self.apply(lambda x: (x - minimum) / (maximum  - minimum))
-        
+
     def apply(self, func, inplace=False):
-        """ Applies a function to each element of the matrix, can return or perform inplace"""
+        """ Applies a function to each element of the matrix, can return or
+            perform inplace
+        """
         if not inplace:
             m = Matrix(self.rows, self.cols)
             for i in range(self.rows):
@@ -151,7 +155,7 @@ class Matrix(object):
             for i in range(self.rows):
                 for j in range(self.cols):
                     self.data[i][j] = func(self.data[i][j])
-                
+
     @staticmethod
     def from_array(arr: list):
         """ Instantiates a 1d matrix from an array"""
@@ -159,13 +163,13 @@ class Matrix(object):
         for i in range(m.rows):
             m.data[i][0] = arr[i]
         return m
-              
+
     @staticmethod
     def random_matrix(rows: int, cols: int):
         """ Matrix of random numbers between 0, 1"""
         m = Matrix(rows, cols)
         return m.apply(lambda x: round((random() * 2) - 1, 3))
-    
+
     @staticmethod
     def matmul(a, b):
         """ Matrix multiplication of 2 matrices"""
@@ -177,7 +181,7 @@ class Matrix(object):
                     ele += a.data[i][k] * b.data[k][j]
                 m.data[i][j] = round(ele, 3)
         return m
-        
+
     @staticmethod
     def I(size: int):
         """ Identity Matrix for a given size"""
@@ -185,7 +189,7 @@ class Matrix(object):
         for i in range(size):
             m.data[i][i] = 1
         return m
-    
+
     @staticmethod
     def Hadamard(a, b):
         """ Calculates the hadamard product of two matrices"""
@@ -194,19 +198,19 @@ class Matrix(object):
             for j in range(a.cols):
                 m.data[i][j] = a.data[i][j] * b.data[i][j]
         return m
-    
+
     @staticmethod
     def scalar_product(a, b):
         """ Calculates the dot product of two matricies"""
         if not same_dimention(a, b):
             raise(Exception)
-    
+
         tot = 0
         for i in range(a.rows):
             for j in range(a.cols):
                 tot += (a.data[i][j] * b.data[i][j])
         return tot
-           
+
     @staticmethod
     def same_dimention(a, b):
         """ Checks the dimensions of two matrices"""
--- NN/NeuralNet.py
+++ NN/NeuralNet.py
@@ -4,18 +4,18 @@ from random import choice
 
 def xor_logic(inputs):
     return [1] if inputs[0] + inputs[1] == 1 else [0]
-    
+
 def and_logic(inputs):
     return [1] if inputs[0] + inputs[1] == 2 else [0]
 
 def or_logic(inputs):
     return [1] if inputs[0] + inputs[1] != 0 else [0]
-    
-bools = [[0, 0], 
-         [0, 1], 
-         [1, 0], 
+
+bools = [[0, 0],
+         [0, 1],
+         [1, 0],
          [1, 1]]
-        
+
 def __sig(x):
     """f(x) = 1 / (1 + e^(-x))"""
     return 1 / (1 + math.e**(-x))
@@ -36,9 +36,9 @@ def __drelu(x):
 class ActivationFunction(object):
     """Activation Function Object"""
     def __init__(self, func, dfunc, name=""):
-        """ 
+        """
         Constructor for activation function object
-        
+
         Inputs:
         -------
             func: executable function
@@ -47,9 +47,11 @@ class ActivationFunction(object):
         self.name = name
         self.func = func
         self.dfunc = dfunc
-    
+
     def __repr__(self):
-        return """{} \n\t{} \n\t{}\n""".format(self.name, self.func.__doc__, self.dfunc.__doc__)
+        return """{} \n\t{} \n\t{}\n""".format(self.name,
+                                               self.func.__doc__,
+                                               self.dfunc.__doc__)
 
 sigmoid = ActivationFunction(__sig, __dsig, 'Sigmoid')
 
@@ -57,95 +59,105 @@ ReLU = ActivationFunction(__relu, __drelu, 'ReLU')
 
 class NeuralNet(object):
     """Neural Network Object"""
-    
-    def __init__(self, input_nodes, hidden_nodes, output_nodes, 
+
+    def __init__(self, input_nodes, hidden_nodes, output_nodes,
                  activation_func=sigmoid, learning_rate=0.1):
         """Constuctor for the neural network"""
         self.input_nodes = input_nodes
         self.hidden_nodes = hidden_nodes
         self.output_nodes = output_nodes
-        
-        self.ih_weights = Matrix.random_matrix(self.hidden_nodes, self.input_nodes)
+
+        self.ih_weights = Matrix.random_matrix(self.hidden_nodes,
+                                               self.input_nodes)
         self.hidden_bias = Matrix(self.hidden_nodes, 1)
-        self.ho_weights = Matrix.random_matrix(self.output_nodes, self.hidden_nodes)
+        self.ho_weights = Matrix.random_matrix(self.output_nodes,
+                                               self.hidden_nodes)
         self.output_bias = Matrix(self.output_nodes, 1)
-        
+
         self.__activation_function = activation_func
         self.__learning_rate = learning_rate
-         
+
     @property
     def activation_function(self):
         return self.__activation_function
-    
+
     @activation_function.setter
     def activation_function(self, activation_func):
         self.__activation_function = activation_func
-    
+
     @property
     def learning_rate(self):
         return self.__learning_rate
-    
+
     @learning_rate.setter
     def learning_rate(self, val):
         self.__learning_rate = val
-        
+
     def feed_forward(self, inputs):
-        """ Applying weights, bias and activation function to 
+        """ Applying weights, bias and activation function to
             input and hiddens layer to get the output.
-            
+
             H = activation(W_ih x I + bias_h)
             O = sigmoid(W_ho x H + bias_o)
-        """        
+        """
         # Hidden nodes values
         hidden = Matrix.matmul(self.ih_weights, inputs)
         hidden.add(self.hidden_bias, inplace=True)
         hidden.apply(self.activation_function.func, inplace=True)
-        
+
         # Output nodes values
         output = Matrix.matmul(self.ho_weights, hidden)
         output.add(self.output_bias, inplace=True)
         output.apply(sigmoid.func, inplace=True)
-        
+
         return hidden, output
-    
+
     def errors(self, output, labels):
         """ Calcultes the errors for the output and hidden layers"""
         labels = Matrix.from_array(labels)
         output_errors = labels.add(output.negative)
         hidden_errors = Matrix.matmul(self.ho_weights.T, output_errors)
-        
+
         return hidden_errors, output_errors
-    
+
     def back_propogate(self, inputs, hidden, output, labels):
-        """ Propogates and assigns the weights back from the outputs to the inputs"""
+        """ Propogates and assigns the weights back from the outputs
+            to the inputs
+        """
         hidden_errors, output_errors = self.errors(output, labels)
-        
+
         output_gradient = output.apply(sigmoid.dfunc)
-        output_gradient = self.learning_rate * Matrix.Hadamard(output_errors, output_gradient)
+        output_gradient = self.learning_rate * Matrix.Hadamard(output_errors,
+                                                               output_gradient)
         delta_w_ho = Matrix.matmul(output_gradient, hidden.T)
-        
+
         hidden_gradient = hidden.apply(self.activation_function.dfunc)
-        hidden_gradient = self.learning_rate * Matrix.Hadamard(hidden_errors, hidden_gradient)
+        hidden_gradient = self.learning_rate * Matrix.Hadamard(hidden_errors,
+                                                               hidden_gradient)
         delta_w_ih = Matrix.matmul(hidden_gradient, inputs.T)
-        
+
         return hidden_gradient, delta_w_ih, output_gradient, delta_w_ho
-        
-        
+
+
     def train(self, inputs, labels, stocastic=True):
-        """ Trains the neural net, with the option of stocastic or batch training"""
+        """ Trains the neural net, with the option of stocastic
+            or batch training
+        """
         #Converting inputs to matrix object
         inputs = Matrix.from_array(inputs)
         hidden, output = self.feed_forward(inputs)
         if stocastic:
-            hidden_gradient, delta_w_ih, output_gradient, delta_w_ho = self.back_propogate(inputs, hidden, output, labels)
+            hidden_gradient, delta_w_ih, output_gradient, delta_w_ho = \
+            self.back_propogate(inputs, hidden, output, labels)
             self.output_bias.add(output_gradient, inplace=True)
             self.ho_weights.add(delta_w_ho, inplace=True)
             self.hidden_bias.add(hidden_gradient, inplace=True)
             self.ih_weights.add(delta_w_ih, inplace=True)
         else:
             return self.back_propogate(inputs, hidden, output, labels)
-    
-    def batch_train(self, inputs_array, label_function=xor_logic, batch_size=100):
+
+    def batch_train(self, inputs_array, label_function=xor_logic,
+                    batch_size=100):
         """ Method used to train the neural net in batches"""
         for i in range(batch_size):
             inputs = choice(inputs_array)
@@ -161,18 +173,18 @@ class NeuralNet(object):
                 delta_w_ih.add(result[1], inplace=True)
                 output_gradient.add(result[2], inplace=True)
                 delta_w_ho.add(result[3], inplace=True)
-            
+
         self.output_bias.add(output_gradient, inplace=True)
         self.ih_weights.add(delta_w_ih, inplace=True)
         self.hidden_bias.add(hidden_gradient, inplace=True)
         self.ho_weights.add(delta_w_ho, inplace=True)
-        
-        
+
+
     def predict(self, inputs):
         """ Method to test the neural net for a given input"""
         inputs = Matrix.from_array(inputs)
         return self.feed_forward(inputs)[1].data[0][0]
-    
+
     def __repr__(self):
         string = ""
         string += "\tInputs: {}".format(self.input_nodes)
@@ -180,5 +192,5 @@ class NeuralNet(object):
         string += "\n\toutput: {}".format(self.output_nodes)
         string += "\n\tActivation: {}".format(self.__activation_function.name)
         string += "\n\tLearning Rate: {}".format(self.__learning_rate)
-        
+
         return string
