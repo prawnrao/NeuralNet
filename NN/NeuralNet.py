d15acac7be2e0f600b7ad0221ffd53df6c249ba1
Pranav Rao <prawnrao97@gmail.com>
2020-04-10 10:37:02 +0530

    updates to xor file: correct output path

--- NN/NeuralNet.py
+++ NN/NeuralNet.py
@@ -2,11 +2,6 @@ from Matrix import Matrix
 import math
 from random import choice
 
-bools = [[0, 0],
-         [0, 1],
-         [1, 0],
-         [1, 1]]
-
 def __sig(x):
     """f(x) = 1 / (1 + e^(-x))"""
     return 1 / (1 + math.e**(-x))
--- xor/xor.py
+++ xor/xor.py
@@ -2,79 +2,27 @@ import sys
 from random import random, choice
 import matplotlib.pyplot as plt
 sys.path.insert(1, '/Users/pranavrao/Documents/playground/NN')
-from NeuralNet import NeuralNet, bools, ReLU, sigmoid
+from NeuralNet import NeuralNet, sigmoid
 
-def xor_logic(inputs):
-    return [1] if inputs[0] + inputs[1] == 1 else [0]
-
-### Batch Test Example (sigmoid) ###
-nn = NeuralNet(2,3,1)
-nn.learning_rate=0.2
-size = 100
-num = 100
-with open( 'batch_sigmoid.txt', 'w') as batch:
-    batch.write('Hyperparameters: \n')
-    batch.write("{}\n".format(nn))
-    batch.write("\tBatch Size: {}\n".format(size))
-    batch.write("\t# of Batches: {}\n\n".format(num))
-    for i in range(num):
-        batch.write('Batch No. {}\n'.format(i+1))
-        nn.batch_train(bools, xor_logic, batch_size=size)
-        for b in bools:
-            batch.write("{}\t".format(b))
-            batch.write("{}\n".format(nn.predict(b)))
-        batch.write('\n\n')
-batch.close()
-
-### Batch Test Example (ReLU) ###
-nn = NeuralNet(2,3,1)
-nn.learning_rate=0.2
-nn.activation_function=ReLU
-size = 100
-num = 100
-with open( 'batch_ReLU.txt', 'w') as batch:
-    batch.write('Hyperparameters: \n')
-    batch.write("{}\n".format(nn))
-    batch.write("\tBatch Size: {}\n".format(size))
-    batch.write("\t# of Batches: {}\n\n".format(num))
-    for i in range(num):
-        batch.write('Batch No. {}\n'.format(i+1))
-        nn.batch_train(bools, batch_size=size)
-        for b in bools:
-            batch.write("{}\t".format(b))
-            batch.write("{}\n".format(nn.predict(b)))
-        batch.write('\n\n')
-batch.close()
-
-### Stocastic Test Example (ReLU) ###
-nn = NeuralNet(2,3,1)
-nn.learning_rate=0.2
-nn.activation_function=ReLU
-size = 10000
-with open( 'stocastic_ReLU.txt', 'w') as stocastic:
-    stocastic.write('Hyperparameters: \n')
-    stocastic.write("{}\n".format(nn))
-    stocastic.write("\tSize: {}\n\n".format(size))
-    for i in range(size):
-        inputs = choice(bools)
-        labels = xor_logic(inputs)
-        nn.train(inputs, labels)
-    for b in bools:
-        stocastic.write("{}\t".format(b))
-        stocastic.write("{}\n".format(nn.predict(b)))
-stocastic.close()
+bools = [
+    [0, 0],
+    [1, 0],
+    [0, 1],
+    [1, 1]
+]
 
 ### Stocastic Test Example (sigmoid) ###
 nn = NeuralNet(2,3,1)
 nn.learning_rate=0.2
+nn.activation_function=sigmoid
 size = 10000
-with open( 'stocastic_sigmoid.txt', 'w') as stocastic:
+with open('/Users/pranavrao/Documents/playground/xor/stocastic_sigmoid.txt', 'w') as stocastic:
     stocastic.write('Hyperparameters: \n')
     stocastic.write("{}\n".format(nn))
     stocastic.write("\tSize: {}\n\n".format(size))
     for i in range(size):
         inputs = choice(bools)
-        labels = xor_logic(inputs)
+        labels = [inputs[0] ^ inputs[1]]
         nn.train(inputs, labels)
     for b in bools:
         stocastic.write("{}\t".format(b))
@@ -91,4 +39,4 @@ plt.colorbar()
 plt.xlabel('x1')
 plt.ylabel('x2')
 plt.title
-plt.savefig( 'prediction.png')
+plt.savefig('/Users/pranavrao/Documents/playground/xor/prediction.png')
